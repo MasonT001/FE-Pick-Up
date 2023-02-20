c68ae8738b8f44a05c08355a758a6732f0e1e653
Mason Trepagnier <mltrepagnier@gmail.com>
2023-02-20 14:27:33 -0600

    adding reactive form to sign up page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponentComponent } from './signup-component/signup-component.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LoginComponentComponent } from './login-component/login-component.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signup-component/signup-component.component.ts
+++ src/app/signup-component/signup-component.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-component',
   templateUrl: './signup-component.component.html',
   styleUrls: ['./signup-component.component.css']
 })
-export class SignupComponentComponent {
+export class SignupComponentComponent implements OnInit{
+  signupForm!: FormGroup;
 
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'fullName': new FormControl(''),
+      'email': new FormControl(''),
+      'password': new FormControl(''),
+      'confirmPassword': new FormControl('')
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signupForm)
+  }
 }
