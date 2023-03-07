a9186c836fad65e9325cacdf77d715c02a355dc5
Mason Trepagnier <mltrepagnier@gmail.com>
2023-03-06 22:31:09 -0600

    moved data into new comps + created 2 new comps

---
+++ src/app/pymk/pymk.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pymk',
+  templateUrl: './pymk.component.html',
+  styleUrls: ['./pymk.component.css']
+})
+export class PymkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
