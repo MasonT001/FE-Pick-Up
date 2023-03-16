01c209cb4bd7f868d9a0fcda0d850074d59f4b5b
Mason Trepagnier <mltrepagnier@gmail.com>
2023-03-16 14:45:55 -0500

    update post

--- src/app/shared/post-model.ts
+++ src/app/shared/post-model.ts
@@ -1,7 +1,7 @@
 
 export class Post { 
     
-    constructor(public imageAddress: string, public postText: string, public postID: number ) { }
+    constructor(public imageAddress: string, public postText: string, public id: number ) { }
 
     // string, public caption: string, public upvotes: number, public id: number, public userId: number, public contestId: number
 } 
